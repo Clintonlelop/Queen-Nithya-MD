04e12ab431aa1120d2418bc73463760f8db10b14
Clintonlelop <132855603+Clintonlelop@users.noreply.github.com>
2023-08-30 04:19:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,30 +8,30 @@ const fs = require('fs-extra')
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
  global.github = 'https://github.com/nipuna15' 
- global.location = 'Sri lanka, Monaragala' 
+ global.location = 'Nigeria, Lagos' 
  global.gurl = 'https://instagram.com/' // add your username 
- global.sudo = process.env.SUDO || '919628516236' 
- global.devs = '919628516236'; 
+ global.sudo = process.env.SUDO || '2348160208114' 
+ global.devs = '2348160208114'; 
  global.website = 'https://github.com/' //wa.me/+91000000000000 
  global.THUMB_IMAGE = process.env.THUMB_IMAGE || '' 
  module.exports = { 
-   botname:   process.env.BOT_NAME === undefined ? '' : process.env.BOT_NAME, 
-   ownername: process.env.OWNER_NAME === undefined ? '' : process.env.OWNER_NAME,  
+   botname:   process.env.BOT_NAME === undefined ? 'ＣＬＩＮＴＯＮ ＢＯＴ' : process.env.BOT_NAME, 
+   ownername: process.env.OWNER_NAME === undefined ? 'ＣＬＩＮＴＯＮ™' : process.env.OWNER_NAME,  
    author:  process.env.PACK_INFO.split(";")[0] === undefined ? '' : process.env.PACK_INFO.split(";")[0], 
-   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
+   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS, 
    packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '-' : process.env.PACK_INFO.split(";")[1],
    sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID, 
-   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION , 
+   autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION , 
    antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD, 
-   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE, 
+   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
+voice  :  process.env.AUTO_VOICE === undefined ? true : process.env.AUTO_VOICE, 
    antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE, 
    readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE, 
    auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER, 
-   HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX, 
+   HANDLERS:  process.env.PREFIX === undefined ? ['/'] : process.env.PREFIX, 
    warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT, 
-   disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM, 
-   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE, 
+   disablepm:  process.env.DISABLE_PM === undefined ? true : process.env.DISABLE_PM, 
+   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? true : process.env.LEVEL_UP_MESSAGE, 
    antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES, 
    antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION, 
    BRANCH: 'main',  
